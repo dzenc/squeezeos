1231c7ede83b1f10cd69f524be8171a7d27f40d4
Ralph Irving <ralph.irving@gmail.com>
2021-03-28 09:37:53 -0400

    wext: include wireless event id when it has a size problem.
    The wext code checks if the event data is within size limits.
    When this check fails a message is logged with violating size.
    This change adds the event id to all devices to help resolve
    the violation.
    
    Disable the Radio fix-wireless-event-too-big patch as it does
    not prevent logging the wireles event too big messages.

--- src/s3c2412/linux-2.6.22/net/wireless/wext.c
+++ src/s3c2412/linux-2.6.22/net/wireless/wext.c
@@ -1226,11 +1226,11 @@ void wireless_send_event(struct net_device *	dev,
 	if (descr->header_type == IW_HEADER_TYPE_POINT) {
 		/* Check if number of token fits within bounds */
 		if (wrqu->data.length > descr->max_tokens) {
-			printk(KERN_ERR "%s (WE) : Wireless Event too big (%d)\n", dev->name, wrqu->data.length);
+			printk(KERN_ERR "%s (WE) : Wireless Event (cmd=0x%04X) too big (%d)\n", dev->name, cmd, wrqu->data.length);
 			return;
 		}
 		if (wrqu->data.length < descr->min_tokens) {
-			printk(KERN_ERR "%s (WE) : Wireless Event too small (%d)\n", dev->name, wrqu->data.length);
+			printk(KERN_ERR "%s (WE) : Wireless Event (cmd=0x%04X) too small (%d)\n", dev->name, cmd, wrqu->data.length);
 			return;
 		}
 		/* Calculate extra_len - extra is NULL for restricted events */
